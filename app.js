91a47244d382b5a9b3848689f4b95630960e3aaa
Leop1n <137943710+LeoP1n@users.noreply.github.com>
2023-08-12 23:45:04 -0300

    V3

--- app.js
+++ app.js
@@ -8,18 +8,16 @@ app.use(express.static("public"))
 
 const port=process.env.PORT||3030
 
-app.get("/home",(req,res)=>{
+app.get("/",(req,res)=>{
     res.sendFile(path.join(__dirname,"view/index.html"))
 })
-app.get("/home/register",(req,res)=>{
+app.get("/register",(req,res)=>{
     res.sendFile(path.join(__dirname,"view/register.html"))
 })
-app.get("/home/login",(req,res)=>{
+app.get("/login",(req,res)=>{
     res.sendFile(path.join(__dirname,"view/login.html"))
 })
 
 app.listen(port,()=>{
     console.log(`Servidor corriendo en ${port}`)
 })
-
-
